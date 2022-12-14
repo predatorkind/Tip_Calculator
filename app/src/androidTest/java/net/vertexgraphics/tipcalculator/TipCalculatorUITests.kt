da35bf97269c69b1f1e47d6ea903e3e2eede13f1
Predatorkind <l.trepinski@gmail.com>
2022-12-14 21:01:07 +0000

    Added tests

---
+++ app/src/androidTest/java/net/vertexgraphics/tipcalculator/TipCalculatorUITests.kt
@@ -0,0 +1,27 @@
+package net.vertexgraphics.tipcalculator
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import net.vertexgraphics.tipcalculator.ui.theme.TipCalculatorTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TipCalculatorUITests {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipCalculatorTheme {
+                TipCalculatorScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        composeTestRule.onNodeWithText("Tip Amount: $2.00").assertExists()
+
+
+    }
+}
--- app/src/main/java/net/vertexgraphics/tipcalculator/MainActivity.kt
+++ app/src/main/java/net/vertexgraphics/tipcalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -144,7 +145,8 @@ fun DefaultPreview() {
  * @param roundUp: flag signifying whether to round the tip up
  * @return: tip amount formatted as currency
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String{
+@VisibleForTesting
+internal fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String{
     var tip = tipPercent / 100 * amount
     if (roundUp)
         tip = kotlin.math.ceil(tip)
